9a01b3b05aef73b5a36d72f284190ff9685ac781
Joseph Garcia <j.garc419@gmail.com>
2018-02-22 16:44:37 -0500

    added basic contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-// import './Contact.css';
+import './Contact.css';
+import Social from './Social';
 
 class Contact extends Component {
   render() {
     return (
-      <div>Contact</div>
+    	<div className="contact">
+    		<h1>Contact</h1>
+    		<p>Thanks for stopping by!</p>
+
+    		<p>
+	    		If you would like to get in touch with me, please send me an email to <a className="red-heart" href="">josephagarc@gmail.com</a> or 
+	    		connect with me through any of my social media channels listed below.
+    		</p>
+    		<hr/>
+    		<Social />
+    	</div>
     );
   }
 }
